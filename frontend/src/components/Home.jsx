deed694504771c45e09f95e19a75656f785ec248
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-18 09:59:21 +0200

    mystart

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../Images/logo.png'
+import back from '../Images/back.png'
+
+function Home() {
+  return (
+    <div className='w-full flex flex-col home h-screen'>
+        <div className="main w-full h-full">
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../Images/logo.png'
+
+function Navbar() {
+  return (
+    <div>
+        <div className="logo">
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
