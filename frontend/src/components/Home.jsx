7d44a4b94bac44959f65ed76d56393093f851890
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-18 19:23:56 +0200

    ho_screen

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import logo from '../Images/logo.png'
 import back from '../Images/back.png'
+import Navbar from './Navbar';
 
 function Home() {
   return (
-    <div className='w-full flex flex-col home h-screen'>
+    <div className='w-full text-white flex flex-col home h-screen'>
         <div className="main w-full h-full">
-
+          <Navbar />
+          <div className="flex flex-col pl-5">
+            <p className="text-2xl font-semibold mt-[10vh]">
+              Welcome ____________________________
+            </p>
+            <h1 className="font-bold text-[4em] w-[60%]">
+              Let's make your world  
+              with seductive cinema
+            </h1>
+            <p className='w-[70%]' 
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cum sunt enim autem, dignissimos ducimus architecto magni consequuntur accusamus nulla dicta magnam error fugit adipisci obcaecati cumque porro ea voluptate.</p>
+            <div className="flex mt-[10vh] items-center w-[40%]">
+              <button className='w-1/2 border-2 h-[9vh]
+              hover:bg-red-800 border-red-800
+                px-[10%] py-[2%]'>Login</button>
+              <button className='w-1/2 border-2 h-[9vh] ml-4 border-red-800 bg-red-800
+                px-[10%] py-[2%]'>Get Started</button>
+            </div>
+          </div>
         </div>
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import logo from '../Images/logo.png'
+import WIWO from '../Images/WIWO.png'
 
 function Navbar() {
   return (
-    <div>
-        <div className="logo">
+    <div className='flex items-center px-3 h-[10vh] w-full justify-between'>
+        <div className="logo flex">
             <img src={logo} alt="" />
-            
+            <img className='scale-50 translate-x-[-30px]' src={WIWO} alt="" />
+        </div>
+        <div className="flex text-white w-[50%] justify-center">
+          <p className='mx-11 cursor-pointer hover:text-pink-800
+            duration-300 text-xl font-semibold'>Home</p>
+          <p className='mx-11 cursor-pointer hover:text-pink-800
+            duration-300 text-xl font-semibold'>Categories</p>
+          <p className='mx-11 cursor-pointer hover:text-pink-800
+            duration-300 text-xl mr-[20%] font-semibold'>Patnership</p>
         </div>
     </div>
   )
