8a3798d1010b9aa650c8881212d5afbb49866145
mugishap <precieuxmugisha@gmail.com>
2022-05-20 21:43:46 +0200

    more

--- backend/controllers/uploadImages.js
+++ backend/controllers/uploadImages.js
@@ -1,3 +1,5 @@
+const { movieSchema } = require('../models/movies')
+const moment = require('moment')
 const cloudinary = require('cloudinary').v2
 require('dotenv').config()
 cloudinary.config({
@@ -5,3 +7,15 @@ cloudinary.config({
     api_secret:process.env.API_SECRET,
     cloud_name:process.env.CLOUD_NAME
 })
+
+exports.addMovie = async(req,res)=>{
+    const date = moment().format("dddd, MMMM Do YYYY");
+    const uploaded_response = await cloudinary.uploader.upload(req.body.movieString,{
+    upload_preset:'wiwo'
+})
+console.log(uploaded_response.secure_url)
+const movie = await movieSchema({
+    date:date,
+    
+})
+}
