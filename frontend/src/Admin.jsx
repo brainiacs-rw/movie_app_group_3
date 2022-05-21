7661c21a36a0eb755a0ac41d4c3f8a349cfd74ee
mugishap <precieuxmugisha@gmail.com>
2022-05-21 12:14:41 +0200

    work

--- frontend/src/Admin.jsx
+++ frontend/src/Admin.jsx
@@ -7,8 +7,11 @@ function Admin() {
 
   function convert() {
     const file = document.querySelector("input[type=file]").files[0];
+    const name = document.querySelector("input[type=file]").value
+    console.log(console.log(name.replace(/(.+)fakepath\\(.+)/g, "$2")));
+    const newName = name.replace(/(.+)fakepath\\(.+)/g, "$2")
+    setMovieName(newName)
     const reader = new FileReader();
-
     reader.addEventListener(
       "load",
       function () {
@@ -22,7 +25,6 @@ function Admin() {
       reader.readAsDataURL(file);
     }
   }
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const api = await fetch("http://localhost:4040/movie/newMovie", {
@@ -44,29 +46,30 @@ function Admin() {
       <div className="m-auto w-1/3 flex flex-col shadow-2xl p-4 items-center">
         <div className="inNav ml-2 mb-3 logo w-full flex flex-col items-center justify-center">
           <form onSubmit={handleSubmit}>
-            <div className="labels">
-              <label>Movie name</label>
-              <input
-                type="text"
-                onChange={(e) => {
-                  setMovieName(e.target.value);
-                }}
-              />
-            </div>
+
             <div className="labels">
               <label>Movie genre</label>
-              <input
-                type="text"
+              <select
+                name="genre"
+                id="genre"
                 onChange={(e) => {
                   setGenre(e.target.value);
                 }}
-              />
+                required={true}
+              >
+                <option value="Action">Action</option>
+                <option value="Horror">Horror</option>
+                <option value="Drama">Drama</option>
+                <option value="Comedy">Comedy</option>
+                <option value="Romance">Romance</option>
+                <option value="Fantasy">Fantasy</option>
+              </select>
             </div>
             <div className="labels">
               <label>Movie</label>
               <input type="file" onChange={convert} />
             </div>
-            <input type="submit"  value={"Submit"}/>
+            <input type="submit" value={"Submit"} />
           </form>
         </div>
       </div>
