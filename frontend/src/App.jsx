bfbfd871f296283513209557c5e412aa82ba96a4
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-21 16:05:03 +0200

    no_prob

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Admin from './Admin'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import DashBoard from './components/Dashboad/DashBoard';
+import MoviewPreview from './components/Dashboad/Preview/MoviewPreview';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
 
           <Route path='/' element={<Home />}/>
           <Route path='/dashboard' element={<DashBoard />}/>
+          <Route path='/preview' element={<MoviewPreview />}/>
         </Routes>
       </BrowserRouter>
     </div>
