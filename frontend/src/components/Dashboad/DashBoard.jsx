f692d112bdbd108f1df7a989a593515dded78a1d
spfvtnjr <turinumugishasouvenir@gmail.com>
2022-05-21 19:09:11 +0200

    fix: removed Sidebar from Dashboard because it was causing infinite loading

--- frontend/src/components/Dashboad/DashBoard.jsx
+++ frontend/src/components/Dashboad/DashBoard.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Admin from "./Admin";
 import SideBar from "./SideBar";
+import Feed from "./Feed";
 
 function DashBoard() {
   const [route, setRooute] = useState("feed");
 
   return (
     <div className="flex text-sm fixed h-screen w-full">
-      <SideBar setRooute={setRooute} />
-       <DashBoard />
+      {/* <SideBar setRooute={setRooute} /> */}
+       <Feed />
       {/* {route === "admin" && <Admin />} */}
     </div>
   );
