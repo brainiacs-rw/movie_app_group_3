8adfe9a712e77f24dd5d9876670d2078075a2ce0
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-21 19:15:42 +0200

    fixed navbar from causing infinite loading

--- frontend/src/components/Dashboad/DashBoard.jsx
+++ frontend/src/components/Dashboad/DashBoard.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import Admin from "./Admin";
+// import Admin from "./Admin";
+import Feed from "./Feed";
 import SideBar from "./SideBar";
 
 function DashBoard() {
-  const [route, setRooute] = useState("feed");
+//   const [route, setRooute] = useState("feed");
 
   return (
     <div className="flex text-sm fixed h-screen w-full">
-      <SideBar setRooute={setRooute} />
-       <DashBoard />
+      <SideBar />
+       <Feed />
       {/* {route === "admin" && <Admin />} */}
     </div>
   );
